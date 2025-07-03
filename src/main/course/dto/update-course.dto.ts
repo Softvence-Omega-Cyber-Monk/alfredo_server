4af3a3909cacf669d76bf6024e8f98ff9bc05e90
rahmanabdulquadir <rahmanaq777@gmail.com>
2025-07-03 15:53:01 +0600

    model updated as necessary

--- src/main/course/dto/update-course.dto.ts
+++
@@ -1,5 +0,0 @@
-// src/main/course/dto/update-course.dto.ts
-import { PartialType } from '@nestjs/swagger';
-import { CreateCourseDto } from './create-course.dto';
-
-export class UpdateCourseDto extends PartialType(CreateCourseDto) {}
