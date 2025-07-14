5ba01f0d9a2233f75c0681a7fb5901b50e01d12a
rahmanabdulquadir <rahmanaq777@gmail.com>
2025-07-14 09:06:47 +0600

    alfredo server resumed

--- src/main.ts
+++ src/main.ts
@@ -34,6 +34,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(process.env.PORT ?? 7002);
+  await app.listen(process.env.PORT ?? 8000);
 }
 bootstrap();
