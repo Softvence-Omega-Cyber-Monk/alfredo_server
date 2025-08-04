504aea08241ce87a59de867879962e43fabe9fce
rahmanabdulquadir <rahmanaq777@gmail.com>
2025-08-04 16:52:00 +0600

    dto files and module file updated

--- src/main/onboarding/onboarding.module.ts
+++ src/main/onboarding/onboarding.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
-import { OnboardingController } from './onboarding.controller';
 import { OnboardingService } from './onboarding.service';
+import { OnboardingController } from './onboarding.controller';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Module({
   controllers: [OnboardingController],
-  providers: [OnboardingService]
+  providers: [OnboardingService, PrismaService],
 })
 export class OnboardingModule {}
