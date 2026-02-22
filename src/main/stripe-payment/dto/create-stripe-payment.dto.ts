a2bc2727597b0fab9996fd963f2d6a52cc9d23b9
sazzad-hosen-mahim <sajjadhosenmahim@gmail.com>
2026-02-23 00:16:42 +0600

    payment dto validation updated

--- src/main/stripe-payment/dto/create-stripe-payment.dto.ts
+++ src/main/stripe-payment/dto/create-stripe-payment.dto.ts
@@ -1,12 +1,16 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNumber, IsString } from 'class-validator';
 
 export class CreateStripePaymentDto {
   @ApiProperty({ example: 'price_1RuIseCiM0crZsfwqv3vZZGj' })
+  @IsString()
   priceId: string;
 
   @ApiProperty({ example: '1503ad99-f64b-4181-8bdb-767def84a517' })
+  @IsString()
   planId: string;
 
   @ApiProperty({ example: 1 })
+  @IsNumber()
   planDuration: number;
 }
