78ca66bc578a5e718981be9d05d3b9b8f25e19a4
sazzad-hosen-mahim <sajjadhosenmahim@gmail.com>
2026-04-04 22:37:51 +0600

    stripe issue fixed

--- src/main/stripe-payment/dto/create-stripe-payment.dto.ts
+++ src/main/stripe-payment/dto/create-stripe-payment.dto.ts
@@ -1,19 +1,29 @@
+// create-stripe-payment.dto.ts
 import { ApiProperty } from '@nestjs/swagger';
 import { IsNumber, IsString, IsNotEmpty } from 'class-validator';
 import { Type } from 'class-transformer';
 
 export class CreateStripePaymentDto {
-  @ApiProperty({ example: 'price_1RuIseCiM0crZsfwqv3vZZGj' })
+  @ApiProperty({
+    description: 'Stripe Price ID for the plan',
+    example: 'price_1RuIseCiM0crZsfwqv3vZZGj',
+  })
   @IsString()
   @IsNotEmpty()
   priceId: string;
 
-  @ApiProperty({ example: '1503ad99-f64b-4181-8bdb-767def84a517' })
+  @ApiProperty({
+    description: 'Internal Plan ID from the database',
+    example: '1503ad99-f64b-4181-8bdb-767def84a517',
+  })
   @IsString()
   @IsNotEmpty()
   planId: string;
 
-  @ApiProperty({ example: 1 })
+  @ApiProperty({
+    description: 'Duration of the plan in years (e.g., 1 or 2)',
+    example: 1,
+  })
   @IsNumber()
   @IsNotEmpty()
   @Type(() => Number)
