bf2ee5f01d427bb8e0ecf7d4a49fbb6de2630083
Sazzad Hosen <108649437+Sazzad-Hosen-Mahim@users.noreply.github.com>
2026-05-31 03:01:44 +0600

    exchange request dto fixed

--- src/main/exchange-request/dto/create-exchange-request.dto.ts
+++ src/main/exchange-request/dto/create-exchange-request.dto.ts
@@ -1,21 +1,28 @@
 // create-exchange-request.dto.ts
 import { ApiProperty } from '@nestjs/swagger';
+import { IsUUID, IsString, IsOptional } from 'class-validator';
 
 export class CreateExchangeRequestDto {
   @ApiProperty({ description: 'ID of the user sending the request' })
+  @IsUUID()
   fromUserId: string;
 
   @ApiProperty({ description: 'ID of the user receiving the request' })
+  @IsUUID()
   toUserId: string;
 
   @ApiProperty({ description: 'ID of the property offered by the sender' })
+  @IsUUID()
   fromPropertyId: string;
 
   @ApiProperty({
     description: 'ID of the property requested from the receiver',
   })
+  @IsUUID()
   toPropertyId: string;
 
   @ApiProperty({ description: 'Optional message' })
+  @IsOptional()
+  @IsString()
   message?: string;
 }
