d3db2a8a201519626b5a25f3407502d6a8397dd2
sazzad-hosen-mahim <sajjadhosenmahim@gmail.com>
2026-06-22 13:55:26 +0600

    user controller changed in terms to change profile form profile picture committing again cause ssh failed

--- src/main.ts
+++ src/main.ts
@@ -22,7 +22,6 @@ async function bootstrap() {
         'http://localhost:3000',
         'http://localhost:3001',
         'http://localhost:4000',
-        'http://localhost:4001',
         'http://localhost:5173',
         'https://vacanza-dashboard.vercel.app',
         'https://admin.vacanzagreece.gr',
